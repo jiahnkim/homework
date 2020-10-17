10f86b47942240da6dbd2899a07368c13d8b9b90
jiahnkim <71361096+jiahnkim@users.noreply.github.com>
2020-10-17 12:56:53 +0900

    재 제출합니다

--- 3주차 숙제.py
+++ 3주차 숙제.py
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+for tr in trs:
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text
+    # print(rank, title, artist)
+
+    doc ={
+        'rank':rank,
+        'title':title,
+        'artist':artist
+    }
+    db.genie.insert_one(doc)
+
+    print(doc)
+
+=======
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
 
 client = MongoClient('localhost', 27017)
@@ -25,3 +56,4 @@ for tr in trs:
     }
     db.genie.find_one(doc)
 
+>>>>>>> f5c476a5935ca66ba471c1c3c151f459fb2b3c8b
